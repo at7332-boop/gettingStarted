a1eb76f48ce0dfc2a3685bc62b4572426fa48c5c
at7332-boop <at7332@nyu.edu>
2026-09-12 21:36:53 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -35,7 +35,7 @@ def welcome_assignment_answers(question):
         answer = "No"
 
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 1
+        answer = 5
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
